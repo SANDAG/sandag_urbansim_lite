a7ff8081bbc481e0f1b7af95a85f4aec2e05f386
Elias Sanz <esa@sandag.org>
2017-08-17 15:29:05 -0700

    drop postgresql, move to sql

--- data/import_data.py
+++ data/import_data.py
@@ -9,55 +9,67 @@ db_connection_string = get_connection_string('config.yml', 'mssql_db')
 mssql_engine = create_engine(db_connection_string)
 
 parcels_sql = '''
-        with temp as (SELECT [ludu2015_parcel_id]
-	  ,p.jurisdiction_id
-      ,[sr13_cap_hs_growth_adjusted] as sr13_cap
-  FROM [urbansim].[ref].[sr13_capacity] s
-  Left join [urbansim].[urbansim].[parcel] p on s.ludu2015_parcel_id = p.parcel_id
-  where p.jurisdiction_id in (14, 19) and sr13_cap_hs_growth_adjusted > 0)
-
-SELECT         p.parcel_id
-			  ,p.jurisdiction_id
-              ,p.[development_type_id] as building_type_id
-              ,coalesce(cast(c.sr14_cap as int), 0) + coalesce(cast(sr13_cap as int), 0) + coalesce(sum(b.residential_units), 0) as total_cap
-              ,coalesce(sum(b.residential_units), 0) as residential_units
-			  ,sr13_cap
-			  ,c.sr14_cap as sr14_cap
-             -- ,[land_value]/ [parcel_acres] as land_value_per_acre
-              FROM [urbansim].[urbansim].[parcel] as p
-              LEFT JOIN [urbansim].[urbansim].[building] as b
-              ON b.parcel_id = p.parcel_id
-              LEFT JOIN staging.sr14_capacity as c
-              ON c.parcel_id = p.parcel_id
-			  LEFT JOIN temp as t
-              ON t.ludu2015_parcel_id = p.parcel_id
-			  where sr13_cap > 0 or sr14_cap > 0
-              GROUP BY  p.[parcel_id], p.jurisdiction_id, c.sr14_cap, sr13_cap, p.[development_type_id]'''
+    WITH temp AS (
+        SELECT
+            ludu2015_parcel_id
+            ,p.jurisdiction_id
+            ,sr13_cap_hs_growth_adjusted AS sr13_cap
+        FROM urbansim.ref.sr13_capacity AS s
+        LEFT JOIN urbansim.urbansim.parcel p ON s.ludu2015_parcel_id = p.parcel_id
+        WHERE p.jurisdiction_id IN (14, 19) AND sr13_cap_hs_growth_adjusted > 0
+    )
+    SELECT
+        p.parcel_id
+        ,p.jurisdiction_id
+        ,p.development_type_id AS building_type_id
+        ,COALESCE(CAST(c.sr14_cap AS int), 0) + COALESCE(CAST(sr13_cap AS int), 0) + COALESCE(b.residential_units, 0) AS total_cap
+        ,COALESCE(b.residential_units, 0) as residential_units
+        ,sr13_cap
+        ,c.sr14_cap as sr14_cap
+        -- ,land_value/ parcel_acres as land_value_per_acre
+    FROM urbansim.urbansim.parcel AS p
+    LEFT JOIN (SELECT parcel_id, SUM(residential_units) AS residential_units FROM urbansim.urbansim.building GROUP BY parcel_id) AS b
+        ON b.parcel_id = p.parcel_id
+    LEFT JOIN staging.sr14_capacity AS c
+        ON c.parcel_id = p.parcel_id
+    LEFT JOIN temp AS t
+        ON t.ludu2015_parcel_id = p.parcel_id
+    WHERE sr13_cap > 0 OR sr14_cap > 0
+'''
 
 parcels_distance_to_coast_sql = '''
-SELECT parcel_id, distance_to_coast
-  FROM urbansim.parcels;
+    SELECT
+        parcel_id
+        ,distance_to_coast
+    FROM urbansim.urbansim.parcel;
 '''
 
 households_sql = '''
-          SELECT yr as year, sum(households) as hh
-            FROM urbansim.household_controls
-            GROUP BY yr
-            ORDER BY yr'''
+    SELECT
+        yr AS year
+        ,sum(households) AS hh
+    FROM urbansim.urbansim.household_control
+    GROUP BY yr
+    ORDER BY yr
+'''
 
 
 ## Missing building_sqft, residential sales price, non_residential rent
-buildings_sql = '''SELECT building_id, parcel_id,
-                          COALESCE(development_type_id,0) as building_type_id,
-                          COALESCE(residential_units, 0) as residential_units,
-                          COALESCE(year_built, 0) year_built
-                     FROM [urbansim].[urbansim].[building]'''
+buildings_sql = '''
+    SELECT
+        building_id
+        ,parcel_id
+        ,COALESCE(development_type_id,0) AS building_type_id
+        ,COALESCE(residential_units, 0) AS residential_units
+        ,COALESCE(year_built, 0) AS year_built
+    FROM urbansim.urbansim.building
+'''
 
 
-households_df = pd.read_sql(households_sql, postgres_engine, index_col='year')
+households_df = pd.read_sql(households_sql, mssql_engine, index_col='year')
 buildings_df = pd.read_sql(buildings_sql, mssql_engine, index_col='building_id')
 parcels_df = pd.read_sql(parcels_sql, mssql_engine, index_col='parcel_id')
-parcels_distance_to_coast_df = pd.read_sql(parcels_distance_to_coast_sql, postgres_engine, index_col='parcel_id')
+parcels_distance_to_coast_df = pd.read_sql(parcels_distance_to_coast_sql, mssql_engine, index_col='parcel_id')
 print(len(parcels_df))
 parcels_df = parcels_df.join(parcels_distance_to_coast_df)
 parcels_df = parcels_df.fillna(1000)
