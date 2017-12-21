0fdb604119897e75fd7ad26cac1d51af46fafb0e
Anne Kuller <aku@sandag.org>
2017-12-21 13:22:07 -0800

    add regionwide controls

--- models.py
+++ models.py
@@ -1,6 +1,5 @@
+import datasources
 import orca
-import dataset
-
 import utils
 
 
@@ -15,17 +14,18 @@ def feasibility(parcels):
 
 
 @orca.step('residential_developer')
-def residential_developer(feasibility, households, buildings, parcels, year):
+def residential_developer(feasibility, households, buildings, parcels, year, regional_controls, jurisdictions):
     utils.run_developer(forms=None,
                         parcels=parcels,
                         agents=households,
                         buildings=buildings,
+                        reg_controls=regional_controls,
+                        jurisdictions=jurisdictions,
                         supply_fname="residential_units",
                         total_units=parcels.residential_units,
                         feasibility=feasibility,
                         year=year,
-                        target_vacancy=.02,
+                        target_vacancy=0.0,
                         form_to_btype_callback=None,
                         add_more_columns_callback=None,
                         bldg_sqft_per_job=400.0)
-
