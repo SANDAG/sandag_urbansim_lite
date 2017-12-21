b65f97e15c79b143704da7bd034b652ab67e83ee
Anne Kuller <aku@sandag.org>
2017-12-21 12:48:56 -0800

    match totals exactly

--- utils.py
+++ utils.py
@@ -3,37 +3,21 @@ from __future__ import print_function
 import numpy as np
 import orca
 from urbansim.developer import developer
+import pandas as pd
 
 
-def profit_to_prob_function(df1, var_list=['distance_to_coast']):
+def largest_remainder_allocation(df, k):
+    ratios = df.control.values
+    frac, results = np.modf(k * ratios)
+    remainder = int(k - results.sum()) # how many left
 
-    """
-    Execute development feasibility on all parcels
+    indices = np.argsort(frac)[::-1]
+    results[indices[0:remainder]] += 1 # add one to the ones with the highest decimal.
 
-    Parameters
-    ----------
-    df : DataFrame Wrapper
-        The data frame wrapper for the parcel data
-    var_list : array
-        List of variables to be used to determine the probability of being picked
-    Returns
-    -------
-    Adds a series with profit function
-    """
-    df1['random'] = np.random.normal(.5, .05, df1.shape[0])
+    df['targets'] = results.astype(int).tolist()
 
-    for x in var_list:
-            df1[x + '_normal'] = ((df1[x].max() - df1[x]) / (df1[x].max() - df1[x].min()))
+    return df
 
-            df1['random'] = (df1['random'] * df1[x + '_normal'])
-
-            df1.drop(x + '_normal', axis=1, inplace=True)
-
-    df1['random_prob'] = (df1.random / df1.random.sum())
-#    df1.drop('random', axis=1, inplace=True)
- #   return df1['random_prob']
-    print ('the length is: ',len(df1))
-    return df1
 
 def run_feasibility(parcels):
     """
@@ -46,18 +30,17 @@ def run_feasibility(parcels):
 
     Returns
     -------
-    Adds a table called feasibility to the sim object (returns nothing)
+    Adds a table called feasibility to the orca object (returns nothing)
     """
 
     print("Computing feasibility")
     parcels = parcels.to_frame()
-    parcels.loc[parcels['distance_to_coast'] > 50000, 'distance_to_coast'] = 50000
+    # feasible parcels have capacity for additional units
     feasible_parcels = parcels.loc[parcels['total_cap'] > parcels['residential_units']]
-    print (feasible_parcels)
     orca.add_table("feasibility", feasible_parcels)
 
 
-def run_developer(forms, parcels, agents, buildings, supply_fname,
+def run_developer(forms, parcels, agents, buildings, reg_controls, jurisdictions, supply_fname,
                   total_units, feasibility, year=None,
                   target_vacancy=.1, form_to_btype_callback=None,
                   add_more_columns_callback=None, max_parcel_size=200000,
@@ -67,8 +50,7 @@ def run_developer(forms, parcels, agents, buildings, supply_fname,
 
     Parameters
     ----------
-    forms : string or list of strings
-        Passed directly dev.pick
+
     parcels : DataFrame Wrapper
         Used to update residential units at the parcel level
     agents : DataFrame Wrapper
@@ -88,21 +70,6 @@ def run_developer(forms, parcels, agents, buildings, supply_fname,
         new buildings
     target_vacancy : float
         The target vacancy rate - used to determine how much to build
-    form_to_btype_callback : function
-        Will be used to convert the 'forms' in the pro forma to
-        'building_type_id' in the larger model
-    add_more_columns_callback : function
-        Takes a dataframe and returns a dataframe - is used to make custom
-        modifications to the new buildings that get added
-    max_parcel_size : float
-        Passed directly to dev.pick - max parcel size to consider
-    residential : boolean
-        Passed directly to dev.pick - switches between adding/computing
-        residential_units and job_spaces
-    bldg_sqft_per_job : float
-        Passed directly to dev.pick - specified the multiplier between
-        floor spaces and job spaces for this form (does not vary by parcel
-        as ave_unit_size does)
 
     Returns
     -------
@@ -110,6 +77,11 @@ def run_developer(forms, parcels, agents, buildings, supply_fname,
     """
 
     dev = developer.Developer(feasibility.to_frame())
+
+    control_totals = reg_controls.to_frame()
+    jurs = jurisdictions.to_frame()
+
+    control_totals_by_year =  control_totals.loc[control_totals.yr == year].copy()
 
     target_units = dev.\
         compute_units_to_build(agents.to_frame().hh.get_value(year),
@@ -119,9 +91,16 @@ def run_developer(forms, parcels, agents, buildings, supply_fname,
     print("{:,} feasible buildings before running developer"
           .format(len(dev.feasibility)))
     df = feasibility.to_frame()
-    p = profit_to_prob_function(df, var_list=[]) #random
-#    p = profit_to_prob_function(df) #distance to coast
-    p1 = p['random_prob']
+
+    # subtract scheduled dev capacity
+    target_units = target_units - df.loc[df['site_id'].notnull()].additional_units.sum()
+
+    df = df.loc[df['site_id'].isnull()].copy()
+
+    print('year is: ', year)
+    print('target_units for region: ', target_units)
+
+    subregional_targets = largest_remainder_allocation(control_totals_by_year, target_units)
 
     '''
         Do not pick or develop if there are no feasible parcels
@@ -134,37 +113,80 @@ def run_developer(forms, parcels, agents, buildings, supply_fname,
         Pick parcels to for new buildings
     '''
 
-#    choices = np.random.choice(df.index.values, size=min(len(df.index), target_units),
- #                              replace=False, p=p.tolist())
-    choices = np.random.choice(p.index.values, size=min(len(p.index), target_units),
-                               replace=False, p=p1.tolist())
-    print (choices)
+    new_buildings= pd.DataFrame()
+    units_per_jur = pd.DataFrame()
+    units_per_j = pd.DataFrame()
+
+    jur_list = jurs['jurisdiction_id'].tolist()
     df['net_units'] = (df.total_cap - df.residential_units)
-    tot_units = df.net_units.loc[choices].values.cumsum()
-    ind = int(np.searchsorted(tot_units, target_units, side="left")) + 1
-    build_idx = choices[:ind]
-    new_buildings = df.loc[build_idx]
-    new_buildings.index.name = "parcel_id"
+    df.net_units = df.net_units.astype(int)
+
+    for jur in jur_list:
+        units = subregional_targets.loc[subregional_targets['geo_id']==jur].targets.values[0]
+        jur_name = jurs.loc[jurs.jurisdiction_id == jur].name.values[0]
+        print("Jurisdiction %d %s target units: %d" % (jur,jur_name,units))
+
+        df_jur = df.loc[df['jurisdiction_id'] == jur].copy()
+
+        one_row_per_unit = df_jur.loc[np.repeat(df_jur.index.values, df_jur.net_units)].copy()
+        one_row_per_unit.reset_index(drop=False,inplace=True)
+        one_row_per_unit['net_units'] = 1
+
+        if len(one_row_per_unit) < units:
+            print("WARNING THERE WERE NOT ENOUGH PROFITABLE UNITS TO",
+                  "MATCH DEMAND FOR ", jur_name,"IN YEAR ",year)
+            choices = one_row_per_unit.index.values
+        elif target_units <= 0:
+            choices = []
+        else:
+            choices = np.random.choice(one_row_per_unit.index.values, size=min(len(one_row_per_unit.index), units),
+                                       replace=False, p=None)
+
+        random_choice_parcels =  one_row_per_unit.loc[choices]
+        # group by parcel id  - one bldg per parcel with multiple units
+        new_bldgs = pd.DataFrame({'count': random_choice_parcels.groupby(["parcel_id","jurisdiction_id", "additional_units","residential_units", "bldgs", "total_cap"]).size()}).reset_index()
+        new_bldgs.rename(columns = {'count': 'net_units'},inplace=True)
+        new_bldgs.set_index('parcel_id',inplace=True)
+
+        new_units = new_bldgs.net_units.sum()
+
+        new_buildings = new_buildings.append(new_bldgs)
+
+
+        dj = {'year': [year], 'jurisdiction': [jur_name], 'target_units_for_jur': [units],
+              'target_units_for_region': [target_units], 'total_units_built_for_jur': [new_units]}
+
+        jur_df = pd.DataFrame(data=dj)
+        units_per_jur = units_per_jur.append(jur_df)
+
+
+    dj = {'year': [year], 'jurisdiction': ['total'],
+          'target_units_for_jur': [units_per_jur.target_units_for_jur.sum()],
+          'target_units_for_region': [target_units], 'total_units_built_for_jur': [units_per_jur.total_units_built_for_jur.sum()]}
+
+    jur_df = pd.DataFrame(data=dj)
+    units_per_jur = units_per_jur.append(jur_df)
+    units_per_j = units_per_j.append(units_per_jur)
+
+    uj = orca.get_table('uj').to_frame()
+    uj = uj.append(units_per_j)
+    orca.add_table("uj", uj)
 
     '''
-       Add new buildings residential units to the parcels by
-       1. Joining parcels and buildings by parcel id
-       2. Adding net_units (new units) to residential parcels
+        Join parcels with parcels that have new units on parcel_id (add net units column)
     '''
-
-    new_buildings["residential_units"] = (new_buildings["net_units"])
-#   new_buildings = new_buildings.drop(['random_prob'], 1)
-
     parcels = parcels.to_frame()
     parcels = parcels.join(new_buildings[['net_units']])
 
-    parcels = parcels.fillna(0)
+    parcels.net_units  = parcels.net_units.fillna(0)
     parcels['residential_units'] = parcels['residential_units'] + parcels['net_units']
     parcels = parcels.drop(['net_units'], 1)
     orca.add_table("parcels", parcels)
 
     new_buildings = new_buildings.reset_index()
-
+    new_buildings['residential_units'] = new_buildings['net_units']
+    # temporarily assign building type id
+    new_buildings['building_type_id'] = 21
     if year is not None:
         new_buildings["year_built"] = year
 
@@ -172,18 +194,10 @@ def run_developer(forms, parcels, agents, buildings, supply_fname,
           .format(len(new_buildings),
                   int(new_buildings[supply_fname].sum()),
                   supply_fname))
-
-    '''
-        Displays number of feasible buildings after dropping parcels where new buildings were built
-    '''
-
-    print("{:,} feasible buildings after running developer"
-          .format(len(dev.feasibility.drop(build_idx))))
-    print (new_buildings.head())
-
     '''
         Merge old building with the new buildings
     '''
+
     all_buildings = dev.merge(buildings.to_frame(buildings.local_columns),
                               new_buildings[buildings.local_columns])
 
