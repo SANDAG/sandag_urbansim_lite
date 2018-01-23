489fe357b5f136b17bd97cf2a1837bc7f6b41c48
Anne Kuller <aku@sandag.org>
2018-01-23 09:49:19 -0800

    increment index in code for units table

--- simulation.py
+++ simulation.py
@@ -30,15 +30,18 @@ SELECT max(run_id)
   FROM [urbansim].[urbansim].[urbansim_lite_output_units]
 '''
 run_id_df = pd.read_sql(run_id_sql, mssql_engine)
-run_id = run_id_df.iloc[0][0]
+run_id = int(run_id_df.values)
 
 index_sql = '''
-SELECT max(index)
+SELECT max([units_index])
   FROM [urbansim].[urbansim].[urbansim_lite_output_units]
 '''
 index_df = pd.read_sql(index_sql, mssql_engine)
-index_id = run_id_df.iloc[0][0]
+max_id = int(index_df.values)
 
+
+buildings_out['units_index'] = range(max_id + 1, max_id + len(buildings_out) + 1)
+# buildings_out['units_index'] = range(1, len(buildings_out)+1)
 
 
 buildings_out['run_id'] = run_id + 1
@@ -48,5 +51,5 @@ buildings_out.to_csv('data/new_units.csv')
 units_by_jur = orca.get_table('uj').to_frame()
 
 units_by_jur.to_csv('data/units_by_jur.csv')
-buildings_out.to_sql(name='urbansim_lite_output_units', con=mssql_engine, schema='urbansim', index=True,if_exists='append')
+# buildings_out.to_sql(name='urbansim_lite_output_units', con=mssql_engine, schema='urbansim', index=False,if_exists='append')
 
