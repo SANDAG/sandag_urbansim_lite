936ee0ca90b3adcce779b50f5ca3eeaf8749896e
SANDAGNET\noz <nick.ozanich@sandag.org>
2018-02-09 14:44:49 -0800

    (ISAM-103) Now adds CPA data and organizes for import to residential control table

--- data/sr13_hu_allocation.py
+++ data/sr13_hu_allocation.py
@@ -51,6 +51,59 @@ sr13_sql_match_san_sched_dev = '''
     ORDER BY yr_from, City
 '''
 
+################################
+# add name to cpa...
+################################
+# San Deigo, jurisdiction_id = 14
+sd_by_cpa = '''
+	SELECT c.name as cpa_name, c.City AS jurisdiction_id,c.cpa, c.yr_from, c.yr_to, c.hu_change
+    FROM (SELECT a.City,a.cpa,a.name, a.increment AS yr_from, b.increment AS yr_to
+    ,CASE WHEN  b.hu - a.hu > 0 THEN b.hu - a.hu ELSE 0 END AS hu_change
+    FROM (SELECT y.cpa,g.name,y.City, x.increment, sum([hs]) AS hu
+    FROM [regional_forecast].[sr13_final].[capacity] x
+    inner join [regional_forecast].[sr13_final].[mgra13] as y on x.mgra = y.mgra
+	inner join data_cafe.ref.geography_zone	as g on g.zone = y.cpa
+    WHERE x.scenario = 0 and x.increment in (2020, 2025, 2030, 2035, 2040, 2045, 2050)
+    and x.site = 0  and g.geography_type_id = 15
+    GROUP BY y.cpa,y.City, x.increment,g.name) AS a
+    inner join 
+    (SELECT y.cpa,y.City, x.increment, sum([hs]) AS hu
+    FROM [regional_forecast].[sr13_final].[capacity] x
+    inner join [regional_forecast].[sr13_final].[mgra13] AS y ON x.mgra = y.mgra
+    WHERE x.scenario = 0 and x.increment in (2020, 2025, 2030, 2035, 2040, 2045, 2050) 
+    and x.site = 0
+    GROUP BY y.cpa,y.City, x.increment) as b
+    ON a.cpa = b.cpa and a.increment = b.increment-5) AS c
+	where City = 14
+    ORDER BY yr_from, City
+    '''
+cpa_sd_df = pd.read_sql(sd_by_cpa, mssql_engine)
+
+# Unincorporated, jurisdiction_id = 19
+unincorp_by_cpa = '''
+	SELECT c.name as cpa_name, c.City AS jurisdiction_id,c.cpa, c.yr_from, c.yr_to, c.hu_change
+    FROM (SELECT a.City,a.cpa,a.name, a.increment AS yr_from, b.increment AS yr_to
+    ,CASE WHEN  b.hu - a.hu > 0 THEN b.hu - a.hu ELSE 0 END AS hu_change
+    FROM (SELECT y.cpa,g.name,y.City, x.increment, sum([hs]) AS hu
+    FROM [regional_forecast].[sr13_final].[capacity] x
+    inner join [regional_forecast].[sr13_final].[mgra13] as y on x.mgra = y.mgra
+	inner join data_cafe.ref.geography_zone	as g on g.zone = y.cpa
+    WHERE x.scenario = 0 and x.increment in (2020, 2025, 2030, 2035, 2040, 2045, 2050)
+    and x.site = 0  and g.geography_type_id = 20
+    GROUP BY y.cpa,y.City, x.increment,g.name) AS a
+    inner join 
+    (SELECT y.cpa,y.City, x.increment, sum([hs]) AS hu
+    FROM [regional_forecast].[sr13_final].[capacity] x
+    inner join [regional_forecast].[sr13_final].[mgra13] AS y ON x.mgra = y.mgra
+    WHERE x.scenario = 0 and x.increment in (2020, 2025, 2030, 2035, 2040, 2045, 2050) 
+    and x.site = 0
+    GROUP BY y.cpa,y.City, x.increment) as b
+    ON a.cpa = b.cpa and a.increment = b.increment-5) AS c
+	where City = 19
+    ORDER BY yr_from, City
+    '''
+cpa_unincorp_df = pd.read_sql(unincorp_by_cpa, mssql_engine)
+
 ##############################################
 # choose with or without sched dev as control
 ##############################################
@@ -98,6 +151,12 @@ sr14_cap_df = pd.read_sql(sr14_cap_sql, mssql_engine,index_col='jurisdiction_id'
 # sr14 units for region
 units_needed = (hh - du - sched_dev_capacity).astype(float)
 
+# add cpa to dataframe
+# drops jurisdictions 14 and 19 to re-add them at CPA level
+add_cpa_df = sr13_hu_df[sr13_hu_df.jurisdiction_id != 14]
+add_cpa_df = add_cpa_df[add_cpa_df.jurisdiction_id != 19]
+sr13_hu_df = pd.concat([add_cpa_df, cpa_sd_df, cpa_unincorp_df], ignore_index=True)
+
 #sr13 units from 2020
 units_needed_sr13_2020 = sr13_hu_df.hu_change.sum()
 
@@ -126,29 +185,49 @@ sr13_hu_df = sr13_hu_df.join(compare_cap_to_forecast,on='jurisdiction_id')
 # adjust hu by capacity differences
 sr13_hu_df['hu_change'] = sr13_hu_df['adj_per'] * sr13_hu_df['hu_change']
 
+# create control percentages per period as a share of total housing unit change in that period
 sr13_hu_df['control'] = sr13_hu_df.hu_change
 number_periods = 0
 for period in sr13_hu_df['yr_from'].unique():
     number_periods = number_periods + 1
     period_allocation = sr13_hu_df.loc[sr13_hu_df['yr_from'] == period].hu_change.sum()
-    print("%d allocation is %d" % (period,period_allocation))
     sr13_hu_df.loc[sr13_hu_df.yr_from == period, 'control'] = sr13_hu_df.control / period_allocation
-print(sr13_hu_df.control.sum()/number_periods, "Should equal 1!")
+# Check calculation for when period length or number is changed:
+print(round(sr13_hu_df.control.sum()/number_periods), "<- Should be equal to 1!")
 
+# split each period into the required number of years
 sr14_res_control = sr13_hu_df.reindex(sr13_hu_df.index.repeat(sr13_hu_df.yr_to - sr13_hu_df.yr_from)).reset_index(drop=True)
-sr14_res_control['scenario'] = 6
+
+# rename for clarity with generalized geography and year incrementing
 sr14_res_control.rename(columns={'jurisdiction_id':'geo_id','yr_from': 'yr'},inplace=True)
 
 # add one to year to start at 2021 and finish 2050 (sched dev before that)
 sr14_res_control['yr'] = sr14_res_control.yr + 1
 
+# modify the year column to increment by one, rather than repeat by period
 while any(sr14_res_control.duplicated()):
     sr14_res_control['year_maker'] = sr14_res_control.duplicated()
     sr14_res_control.loc[sr14_res_control.year_maker == True, 'yr'] = sr14_res_control.yr + 1
 
-sr14_res_control.sort_values(by=['yr','geo_id'],inplace=True)
+# organize by year and geography
+sr14_res_control.sort_values(by=['yr','geo_id','cpa'],inplace=True)
+
+# label each row by geography type
 sr14_res_control['geo'] = 'jurisdiction'
+sr14_res_control.loc[(sr14_res_control.geo_id == 14) | (sr14_res_control.geo_id == 19), 'geo'] = 'cpa'
+
+# set geography_id for sub-jurisdiction regions
+sr14_res_control.loc[sr14_res_control.cpa.notnull(), 'geo_id'] = sr14_res_control.cpa
+sr14_res_control.geo_id = sr14_res_control.geo_id.astype(int)
+
+# formalize dataframe for exporting
+########################################
+## Be sure to change scenario!
+# scenario 7 has cpa for San Diego and Unincorporated (jurisdiction_id 14 and 19)
+########################################
+sr14_res_control['scenario'] = 7
 sr14_res_control['control_type'] = 'percentage'
+sr14_res_control = sr14_res_control.reset_index()
 
 # to write to csv
 # sr14_res_control.to_csv('sr14_res_control.csv')
@@ -158,28 +237,3 @@ sr14_res_control = sr14_res_control[['scenario','yr','geo','geo_id','control','c
 
 # to write to database
 # sr14_res_control.to_sql(name='residential_control', con=mssql_engine, schema='urbansim', index=False,if_exists='append')
-
-
-# add name to cpa...
-city_by_cpa = '''
-	SELECT '' as jurisdiction, c.name as CPA_name, c.City AS jurisdiction_id,c.CPA, c.yr_from, c.yr_to, c.hu_change
-    FROM (SELECT a.City,a.CPA,a.name, a.increment AS yr_from, b.increment AS yr_to
-    ,CASE WHEN  b.hu - a.hu > 0 THEN b.hu - a.hu ELSE 0 END AS hu_change
-    FROM (SELECT y.CPA,g.name,y.City, x.increment, sum([hs]) AS hu
-    FROM [regional_forecast].[sr13_final].[capacity] x
-    inner join [regional_forecast].[sr13_final].[mgra13] as y on x.mgra = y.mgra
-	inner join data_cafe.ref.geography_zone	as g on g.zone = y.CPA
-    WHERE x.scenario = 0 and x.increment in (2020, 2025, 2030, 2035, 2040, 2045, 2050)
-    and x.site = 0  and  g.geography_type_id = 15 
-    GROUP BY y.CPA,y.City, x.increment,g.name) AS a
-    inner join 
-    (SELECT y.CPA,y.City, x.increment, sum([hs]) AS hu
-    FROM [regional_forecast].[sr13_final].[capacity] x
-    inner join [regional_forecast].[sr13_final].[mgra13] AS y ON x.mgra = y.mgra
-    WHERE x.scenario = 0 and x.increment in (2020, 2025, 2030, 2035, 2040, 2045, 2050) 
-    and x.site = 0
-    GROUP BY y.CPA,y.City, x.increment) as b
-    ON a.CPA = b.CPA and a.increment = b.increment-5) AS c
-	where City = 14
-    ORDER BY yr_from, City
-    '''
