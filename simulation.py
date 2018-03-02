257d3eb2769ff06f11a0791b8b2c18798ce462ba
SANDAGNET\noz <nick.ozanich@sandag.org>
2018-03-02 15:38:29 -0800

    Update simulation to create run_id information table

--- simulation.py
+++ simulation.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 from pysandag.database import get_connection_string
 import pandas as pd
 import sqlalchemy
+import datetime
 
 
 utils.initialize_tables()
@@ -33,11 +34,13 @@ SELECT max(run_id)
   FROM [urbansim].[urbansim].[urbansim_lite_output]
 '''
 run_id_df = pd.read_sql(run_id_sql, mssql_engine)
-run_id = int(run_id_df.values)
+try:
+    run_id = int(run_id_df.values) + 1
+except:
+    run_id = int(1)
 
+buildings_out['run_id'] = run_id
 
-buildings_out['run_id'] = run_id + 1
-# buildings_out['run_id'] = 1
 buildings_out.to_csv('data/new_units.csv')
 
 units_by_jur = orca.get_table('uj').to_frame()
@@ -48,3 +51,9 @@ units_by_jur.to_csv('data/units_by_jur.csv')
 #                    dtype = {'parcel_id': sqlalchemy.types.INTEGER(),'units_added': sqlalchemy.types.INTEGER(),
 #                             'year_simulation': sqlalchemy.types.INTEGER(), 'source': sqlalchemy.types.VARCHAR(length=50),
 #                             'run_id': sqlalchemy.types.INTEGER()})
+#
+# output_records = pd.DataFrame(columns=['run_id', 'run_description', 'run_date'])
+# run_description = 'Test sched_dev; jur=14,19 cap not updated; jur=8 max_units 100/year'
+# run_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+# output_records.loc[run_id] = [run_id, run_description, run_date]
+# output_records.to_sql(name='urbansim_lite_output_runs', con=mssql_engine, schema='urbansim', index=False, if_exists='append')
