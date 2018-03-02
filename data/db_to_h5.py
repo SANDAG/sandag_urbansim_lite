ab0d9919b1e711b8a3e6ef27b092697a49cdf6e8
SANDAGNET\noz <nick.ozanich@sandag.org>
2018-03-02 11:05:48 -0800

    Keep buildings database size minimal to speed up code

--- data/db_to_h5.py
+++ data/db_to_h5.py
@@ -85,6 +85,7 @@ buildings_sql = '''
         ,COALESCE(residential_units,0) AS residential_units
         ,COALESCE(year_built,0) AS year_built
      FROM urbansim.urbansim.building
+     where year_built > 2015
 '''
 
 regional_capacity_controls_sql = '''
