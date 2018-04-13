48579974e2cb180da5f91b4c359d5a2d41d3ffd6
SANDAGNET\noz <nick.ozanich@sandag.org>
2018-04-13 16:57:46 -0700

    Can now fully write out to sql server. Possibly a bug in code (produces 19 extra rows)

--- bulk_insert.py
+++ bulk_insert.py
@@ -49,7 +49,7 @@ def year_update_formater(parcel_table, current_builds, phase_year, scenario, yea
     year_update['phase'] = year_update['phase'].fillna(2015)
     year_update.loc[:, year_update.isnull().any() == False] = year_update.loc[:,
                                                               year_update.isnull().any() == False].astype(int)
-    year_update = year_update[['scenario', 'increment', 'parcel_id', 'year', 'jur', 'jur_reported', 'cpa', 'mgra',
+    year_update = year_update[['scenario', 'increment', 'parcel_id', 'year', 'jurisdiction_id', 'cap_jurisdiction_id', 'cpa', 'mgra',
                                'luz', 'taz', 'site_id', 'lu', 'plu', 'hs', 'chg_hs', 'cap_hs', 'source', 'phase']]
     year_update.sort_values(by=['parcel_id'])
     year_update = year_update.reset_index(drop=True)
@@ -75,8 +75,8 @@ def run_insert(year):
                 [increment] [smallint] NOT NULL,
                 [parcel_id] [int] NOT NULL,
                 [year] [smallint] NOT NULL,
-                [jur] [tinyint] NOT NULL,
-                [jur_reported] [tinyint] NOT NULL,
+                [jurisdiction_id] [tinyint] NOT NULL,
+                [cap_jurisdiction_id] [tinyint] NOT NULL,
                 [cpa] [smallint] NULL,
                 [mgra] [smallint] NULL,
                 [luz] [tinyint] NULL,
@@ -105,7 +105,10 @@ def run_insert(year):
             FROM [urbansim].[urbansim].[sr14_residential_CAP_parcel_results]
         '''
         scenario_df = pd.read_sql(scenario_sql, mssql_engine)
-        scenario = int(scenario_df.values)
+        try:
+            scenario = int(scenario_df.values)
+        except TypeError:
+            scenario = int(1)
     # if year == 2017: # this if / else can be used for appending to the table
     #     try:
     #         scenario_sql = '''
@@ -169,6 +172,64 @@ def run_insert(year):
     ##########################################################
     # End of upload section, below is the actual bulk insert #
     ##########################################################
+    # start_time = time.monotonic()
+    conn = mssql_engine.connect()
+    if year == 2017:
+        with conn.begin() as trans:
+            conn.execute('DROP TABLE IF EXISTS urbansim.sr14_residential_parcel_staging')
+        with conn.begin() as trans:
+            staging_table_sql = '''
+                IF OBJECT_ID(N'urbansim.sr14_residential_parcel_staging', N'U') IS NULL
+                CREATE TABLE [urbansim].[sr14_residential_parcel_staging](
+                   [scenario] [varchar](50) NOT NULL,
+                   [increment] [varchar](50) NOT NULL,
+                   [parcel_id] [varchar](50) NOT NULL,
+                   [year] [varchar](50) NOT NULL,
+                   [jurisdiction_id] [varchar](50) NOT NULL,
+                   [cap_jurisdiction_id] [varchar](50) NOT NULL,
+                   [cpa] [float] NULL,
+                   [mgra] [varchar](50) NULL,
+                   [luz] [varchar](50) NULL,
+                   [taz] [varchar](50) NULL,
+                   [site_id] [float] NULL,
+                   [lu] [varchar](50) NULL,
+                   [plu] [varchar](50) NULL,
+                   [hs] [varchar](50) NOT NULL,
+                   [chg_hs] [varchar](50) NOT NULL,
+                   [cap_hs] [varchar](50) NOT NULL,
+                   [source] [varchar](50) NOT NULL,
+                   [phase] [varchar](50) NULL
+                   )'''
+            conn.execute(staging_table_sql)
+        # The following execute should be taken out if you don't want to clear the table every time you re-run
+        with conn.begin() as trans:
+            conn.execute('TRUNCATE TABLE urbansim.urbansim.sr14_residential_CAP_parcel_results')
+    else:
+        with conn.begin() as trans:
+            conn.execute('TRUNCATE TABLE urbansim.sr14_residential_parcel_staging')
+
+    # This inserts the data first to staging, then into the target table
+    # Needs to be modified to insert into the all_parcels table. Currently only for CAP_parcels
+    with conn.begin() as trans:
+        bulk_insert_staging_sql = '''
+            BULK INSERT urbansim.sr14_residential_parcel_staging
+            FROM '\\\\sandag.org\\home\\shared\\TEMP\\noz\\outputs\\year_update_{}.csv'
+            WITH (FIRSTROW = 2, FIELDTERMINATOR = ',')
+            '''.format(year)
+        conn.execute(bulk_insert_staging_sql)
+    with conn.begin() as trans:
+        staging_to_target_sql = '''
+            INSERT INTO urbansim.urbansim.sr14_residential_CAP_parcel_results
+            SELECT * FROM urbansim.sr14_residential_parcel_staging
+            '''
+        conn.execute(staging_to_target_sql)
+    conn.close()
+    # end_time = time.monotonic()
+    # print(timedelta(seconds=end_time - start_time))
+
+    ###############################
+    # Can ignore everything below #
+    ###############################
     '''
     To create format file, use the following script in the command shell from the folder where you want the 
     format file to be located ("M:\\RES\\estimates & forecast\\SR14 Forecast\\UrbanSim\\Capacity_Parcel_Updates"): 
@@ -177,16 +238,37 @@ def run_insert(year):
     You need to open the .fmt file and edit the top row to be 13.0 instead of 14.0. This should be possible by adding
     -V 130 to the original bcp command but it was giving me an error.
     '''
-    start_time = time.monotonic()
-    conn = mssql_engine.connect()
-    with conn.begin() as trans:
-        bulk_insert = '''
-        BULK INSERT urbansim.urbansim.sr14_residential_CAP_parcel_results
-        FROM '\\\\sandag.org\\home\\shared\\TEMP\\noz\\outputs\\year_update_{}.csv'
-        WITH (FIRSTROW = 2, 
-        FORMATFILE = '\\\\sandag.org\\home\\shared\\TEMP\\noz\\outputs\\parcels_update.fmt')
-        '''.format(year)
-        conn.execute(bulk_insert)
-    conn.close()
-    end_time = time.monotonic()
-    print(timedelta(seconds=end_time - start_time))
+    # start_time = time.monotonic()
+    # conn = mssql_engine.connect()
+    # with conn.begin() as trans:
+    #     bulk_insert = '''
+    #     BULK INSERT urbansim.urbansim.sr14_residential_CAP_parcel_results
+    #     FROM '\\\\sandag.org\\home\\shared\\TEMP\\noz\\outputs\\year_update_{}.csv'
+    #     WITH (FIRSTROW = 2,
+    #     FORMATFILE = '\\\\sandag.org\\home\\shared\\TEMP\\noz\\outputs\\parcels_update.fmt')
+    #     '''.format(year)
+    #     conn.execute(bulk_insert)
+    # conn.close()
+    # end_time = time.monotonic()
+    # print(timedelta(seconds=end_time - start_time))
+
+    # INSERT INTO urbansim.urbansim.sr14_residential_CAP_parcel_results
+    # SELECT CAST(scenario as tinyint)
+    # ,CAST(increment as smallint)
+    # ,CAST(parcel_id as int)
+    # ,CAST(year as smallint)
+    # ,CAST(jurisdiction_id as tinyint)
+    # ,CAST(cap_jurisdiction_id as tinyint)
+    # ,CAST(cpa as smallint)
+    # ,CAST(mgra as smallint)
+    # ,CAST(luz as tinyint)
+    # ,CAST(taz as tinyint)
+    # ,CAST(site_id as smallint)
+    # ,CAST(lu as tinyint)
+    # ,CAST(plu as tinyint)
+    # ,CAST(hs as smallint)
+    # ,CAST(chg_hs as smallint)
+    # ,CAST(cap_hs as smallint)
+    # ,CAST(source as tinyint)
+    # ,CAST(phase as smallint)
+    # FROM urbansim.sr14_residential_parcel_staging
