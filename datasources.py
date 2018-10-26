e673e01a807308a2ac19800e4c7c6c4a778acfc9
SANDAGNET\noz <nick.ozanich@sandag.org>
2018-10-26 10:12:45 -0700

    Added note about separate .h5 files

--- datasources.py
+++ datasources.py
@@ -4,6 +4,14 @@ import bulk_insert
 
 
 # The .h5 file to match run_id=109 is saved at M:\RES\estimates & forecast\SR14 Forecast\UrbanSim\urbansim_v109.h5
+# The .h5 file to match run_id=390 is saved at M:\RES\estimates & forecast\SR14 Forecast\UrbanSim\urbansim_v390.h5
+# 109 was used to create datasource_id 14, 390 was used to create datasource_id 17
+
+# When using the M: drive saved files, the input versions recorded in [urbansim].[urbansim].[urbansim_lite_output_runs]
+# may not actually correspond to the versions used to create the .h5 file. While we try to make them match for record
+# keeping purposes, those saved inputs are not actually used in the active run. We may add a column to this table to
+# reflect this, or somehow indicate this for record keeping purposes. However, the original version (ie. 390) will
+# have accurate version ids recorded, since it was created with the regular .h5 methodology.
 orca.add_injectable("store", pd.HDFStore('M:\\RES\\estimates & forecast\\SR14 Forecast\\UrbanSim\\urbansim_v390.h5', mode="r"))
 
 
