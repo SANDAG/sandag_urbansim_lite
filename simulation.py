1a4235d7e2d056b5fe8aff94a88eec56eba5584a
Anne Kuller <aku@sandag.org>
2019-01-18 10:38:03 -0800

    write csv for debugging

--- simulation.py
+++ simulation.py
@@ -40,6 +40,12 @@ if write_results_to_db == 'y':
 # Save the end time to track how long the program takes to run from start to finish
 end_time = time.monotonic()
 
+hu_forecast = orca.get_table('hu_forecast').to_frame()
+hu_forecast_out = hu_forecast[['parcel_id', 'units_added', 'year_built', 'source', 'capacity_type']].copy()
+hu_forecast_out.rename(columns={'year_built': 'year_simulation'}, inplace=True)
+hu_forecast_out.rename(columns={'units_added': 'unit_change'}, inplace=True)
+hu_forecast_out.to_csv('data/new_units.csv')
+
 # Display total model run time
 print("Total time to run Simulation:", timedelta(seconds=round(end_time - start_time, 2)))
 # Typical times (as of 06/06/2018):
