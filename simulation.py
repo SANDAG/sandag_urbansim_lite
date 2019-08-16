364a77f61e7e5cb0c94fd1a148dda447fd38dec4
SANDAGNET\noz <nick.ozanich@sandag.org>
2019-08-16 13:09:26 -0700

    remove old write-out statements

--- simulation.py
+++ simulation.py
@@ -42,14 +42,9 @@ if write_results_to_db == 'y':
     run_id = utils.add_run_to_db()
     utils.write_results(run_id)
 
-# Save the end time to track how long the program takes to run from start to finish
-end_time = time.monotonic()
-
 hu_forecast = orca.get_table('hu_forecast').to_frame()
 hu_forecast_out = hu_forecast[['parcel_id', 'units_added', 'year_built', 'source', 'capacity_type']].copy()
 hu_forecast_out.rename(columns={'year_built': 'year_simulation'}, inplace=True)
 hu_forecast_out.rename(columns={'units_added': 'unit_change'}, inplace=True)
 hu_forecast_out.to_csv('data/new_units.csv')
 
-# Display total model run time
-print("Total time to run Simulation:", timedelta(seconds=round(end_time - start_time, 2)))
